7e54da2a873f65ab38dce45a6b06975ac69ece48
VitorAlex-97 <amandapereira.dev@gmail.com>
2022-09-03 17:06:10 -0300

    Rotas de entrar e cadastrar

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,10 @@
+import { User } from "./User"
+
+export class Postagem{
+    public id: number
+    public titulo: string
+    public texto: string
+    public data: Date
+    public usuario: User
+    public tema: string
+}
