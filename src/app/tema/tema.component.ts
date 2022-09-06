0e8bcc1684af1b1eaa1098f6d196f8521deea5b4
VitorAlex-97 <amandapereira.dev@gmail.com>
2022-09-05 22:15:24 -0300

    Componente tema

---
+++ src/app/tema/tema.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
+})
+export class TemaComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == ''){
+      alert("Sua sessão expirou, faça o login novamente!")
+      this.router.navigate(['/entrar'])
+    }
+  }
+
+}
