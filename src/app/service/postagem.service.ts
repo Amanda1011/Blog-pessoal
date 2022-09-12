029199b1cf8b90352778ba75c24e016ae427e523
VitorAlex-97 <amandapereira.dev@gmail.com>
2022-09-12 08:29:50 -0300

    Put e delete de postagem

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -22,8 +22,20 @@ export class PostagemService {
       return this.http.get<Postagem[]>(`${this.baseUrl}/postagens`, this.token)
     }
 
+    getByIdPostagem(id: number): Observable<Postagem> {
+      return this.http.get<Postagem>(`${this.baseUrl}/postagens/${id}`, this.token)
+    }
+
     postPostagem(postagem: Postagem): Observable<Postagem>{
       console.log(this.token.headers)
       return this.http.post<Postagem>(`${this.baseUrl}/postagens`, postagem, this.token)
+    }
+
+    putPostagem(postagem: Postagem): Observable<Postagem> {
+      return this.http.put<Postagem>(`${this.baseUrl}/postagens`, postagem, this.token)
+    }
+
+    deletePostagem(id: number) {
+      return this.http.delete(`${this.baseUrl}/postagens/${id}`, this.token)
     }
 }
