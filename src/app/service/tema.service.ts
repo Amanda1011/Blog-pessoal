c913aa9ce3abddc8fee8b3b96d825282a4034625
VitorAlex-97 <amandapereira.dev@gmail.com>
2022-09-20 11:31:06 -0300

    Editando localhost

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -22,26 +22,26 @@ export class TemaService {
 
   getAllTema(): Observable<Tema[]>{
     console.log(this.token.headers)
-    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
+    return this.http.get<Tema[]>('https://mands-blog.netlify.app/tema', this.token)
   }
 
   getByIdTema(id: number): Observable<Tema>{
-    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+    return this.http.get<Tema>(`https://mands-blog.netlify.app/tema/${id}`, this.token)
   }
 
   getByNomeTema(descricao: string): Observable<Tema[]>{
-    return this.http.get<Tema[]>(`http://localhost:8080/tema/descricao/${descricao}`, this.token)
+    return this.http.get<Tema[]>(`https://mands-blog.netlify.app/tema/descricao/${descricao}`, this.token)
   }
 
   postTema(tema: Tema): Observable<Tema>{
-    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+    return this.http.post<Tema>('https://mands-blog.netlify.app/tema', tema, this.token)
   }
 
   putTema(tema: Tema): Observable<Tema>{
-    return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
+    return this.http.put<Tema>('https://mands-blog.netlify.app/tema', tema, this.token)
   }
 
   deleteTema(id: number){
-    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+    return this.http.delete(`https://mands-blog.netlify.app/tema/${id}`, this.token)
   }
 }
